7908eb433e282c0b3d7a0693c3bdad266a977b87
hrz07 <hridoy.developer@yahoo.com>
2022-04-16 01:25:33 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/logo2.png'
+import { BsCart3 } from 'react-icons/bs';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+                <BsCart3 className='cart'/>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjdv3t3gUAxKMllJzOjAnpgA1bK9Ze7w8",
+  authDomain: "red-onion-2234f.firebaseapp.com",
+  projectId: "red-onion-2234f",
+  storageBucket: "red-onion-2234f.appspot.com",
+  messagingSenderId: "363753136174",
+  appId: "1:363753136174:web:de40960e32af8162137fbf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
