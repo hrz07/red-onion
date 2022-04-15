8109812b3d87c1bf3f95e0e5dfb902919e6312a9
hrz07 <hridoy.developer@yahoo.com>
2022-04-16 02:19:09 +0600

    banner complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,6 +5,11 @@ const Home = () => {
     return (
         <div>
             <div className='bannerBox'>
+                <h1>Best food waiting for your belly</h1>
+                <div className="searchBox">
+                    <input type="text" placeholder='Search...' />
+                    <button>Search</button>
+                </div>
             </div>
         </div>
     );
